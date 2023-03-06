a0dad6af7dee9312d17b75f23455f9afd65046d9
priyankaBprabhu <priyankaprabhu96@gmail.com>
2023-03-06 23:29:15 +0530

    Added welcome message on master branch

---
+++ src/main/java/com/bl/hotelreservation/HotelMain.java
@@ -0,0 +1,7 @@
+package com.bl.hotelreservation;
+
+public class HotelMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System");
+    }
+}
